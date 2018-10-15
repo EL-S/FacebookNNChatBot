2c69c9de420f9b3f9c76dfb14562d3fabce80e46
EL-S <31227083+EL-S@users.noreply.github.com>
2018-10-15 23:24:45 +1100

    Allow none power users
    None power users can now request youtube videos

--- facebook_messager.py
+++ facebook_messager.py
@@ -99,49 +99,74 @@ class EchoBot(Client):
                     self.send(Message(text=youtube_vid), thread_id=thread_id, thread_type=thread_type)
             except:
                 self.send(Message(text="ono there was a wiw fuckie wuckie uwu!!"), thread_id=thread_id, thread_type=thread_type)
+        try:
+            if (thread_id in paused_id) or ((paused == True) and (thread_id not in resumed_id)):
+                status = False
+            else: #if it's not in paused or if it is globally paused and this thread is resumed
+                status = True #send message confirmed
+            if message_object.text.lower()[0] == "!":
+                try: #needs refactoring
+                    if (message_object.text.lower().split(" ")[0] == "!yt"):
+                        youtube_link = "https://www.youtube.com/results?search_query="
+                        suffix = ""
+                        for x in message_object.text.lower().split(" "):
+                            if x != "!yt":
+                                suffix += "+"+x
+                        suffix = suffix[1:]
+                        url = youtube_link+suffix
+                        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+                        page = urlopen(req)
+                        soup = BeautifulSoup(page, 'html.parser')
+                        youtube_vids = soup.findAll('a')
+                        first = True
+                        for i in youtube_vids: # super inefficient but I'm lazy
+                            if (i.get("href")[:9] == "/watch?v=") and (first != False):
+                                youtube_vid = "https://www.youtube.com"+i.get("href")
+                                first = False
+                        client.setTypingStatus(TypingStatus.TYPING, thread_id=thread_id, thread_type=thread_type)
+                        self.send(Message(text=youtube_vid), thread_id=thread_id, thread_type=thread_type)
+                except:
+                    self.send(Message(text="ono there was a wiw fuckie wuckie uwu!!"), thread_id=thread_id, thread_type=thread_type)
+            elif (author_id != self.uid) and (status == True): #only processes the messages if not paused
+                current_time = time.time()
+                if time_since < prev_wait_time:
+                    additional_wait = prev_wait_time - time_since
+                print("H:",message_object.text)
+                reply = chatbot_recieve(message_object.text)
+                if message_time_sent:
+                    time_since = current_time - message_time_sent
+                    print(time_since)
+                if grace_period == False:
+                    wait_for = randint(1,2) + additional_wait
+                    print(wait_for)
+                    t = threading.Timer(wait_for, send_async_message, [self, author_id, message_object, thread_id, thread_type, reply])
+                    t.start()  # after wait_for seconds, the message will begin to be sent
+                elif time_since < 60:
+                    wait_for = randint(3,5) + additional_wait
+                    print(wait_for)
+                    t = threading.Timer(wait_for, send_async_message, [self, author_id, message_object, thread_id, thread_type, reply])
+                    t.start()  # after wait_for seconds, the message will begin to be sent
+                else:
+                    wait_for = randint(5,8) + additional_wait
+                    print(wait_for)
+                    t = threading.Timer(wait_for, send_async_message, [self, author_id, message_object, thread_id, thread_type, reply])
+                    t.start()  # after wait_for seconds, the message will begin to be sent
+                read_message = threading.Timer(int(wait_for/2), read_message_function, [self, author_id, message_object, thread_id, thread_type, reply])
+                print("Seen wait:",int(wait_for/2))
+                read_message.start()
+                grace_period = False
+                grace_time = randint(12,30)
+                grace_timestamp = time.time()
+                print("Grace_time:",grace_time)
+                count_down = threading.Timer(grace_time, grace_period_counter)
+                count_down.start()
+                c = 0
+                prev_time = time.time()
+                prev_wait_time = wait_for
+        except:
+            pass
+            #self.send(Message(text="ono there was a wiw fuckie wuckie uwu!!"), thread_id=thread_id, thread_type=thread_type)
 
-        if (thread_id in paused_id) or ((paused == True) and (thread_id not in resumed_id)):
-            status = False
-        else: #if it's not in paused or if it is globally paused and this thread is resumed
-            status = True #send message confirmed
-        
-        print(paused, paused_thread, status)
-        if (author_id != self.uid) and (status == True): #only processes the messages if not paused
-            current_time = time.time()
-            if time_since < prev_wait_time:
-                additional_wait = prev_wait_time - time_since
-            print("H:",message_object.text)
-            reply = chatbot_recieve(message_object.text)
-            if message_time_sent:
-                time_since = current_time - message_time_sent
-                print(time_since)
-            if grace_period == False:
-                wait_for = randint(1,2) + additional_wait
-                print(wait_for)
-                t = threading.Timer(wait_for, send_async_message, [self, author_id, message_object, thread_id, thread_type, reply])
-                t.start()  # after wait_for seconds, the message will begin to be sent
-            elif time_since < 60:
-                wait_for = randint(3,5) + additional_wait
-                print(wait_for)
-                t = threading.Timer(wait_for, send_async_message, [self, author_id, message_object, thread_id, thread_type, reply])
-                t.start()  # after wait_for seconds, the message will begin to be sent
-            else:
-                wait_for = randint(5,8) + additional_wait
-                print(wait_for)
-                t = threading.Timer(wait_for, send_async_message, [self, author_id, message_object, thread_id, thread_type, reply])
-                t.start()  # after wait_for seconds, the message will begin to be sent
-            read_message = threading.Timer(int(wait_for/2), read_message_function, [self, author_id, message_object, thread_id, thread_type, reply])
-            print("Seen wait:",int(wait_for/2))
-            read_message.start()
-            grace_period = False
-            grace_time = randint(12,30)
-            grace_timestamp = time.time()
-            print("Grace_time:",grace_time)
-            count_down = threading.Timer(grace_time, grace_period_counter)
-            count_down.start()
-            c = 0
-            prev_time = time.time()
-            prev_wait_time = wait_for
 
 def read_message_function(self, author_id, message_object, thread_id, thread_type, reply, **kwargs):
     self.markAsRead(thread_id)
