de4b6d8180dd04ab50bc606dcea483a2c8d7412c
EL-S <31227083+EL-S@users.noreply.github.com>
2018-10-18 00:34:27 +1100

    Reimplemented NeuralNetwork Bot
    Responds to non-commands by a non-admin

--- revision.py
+++ revision.py
@@ -11,6 +11,16 @@ from urllib.parse import quote_plus
 
 resumed_id, paused_id = [], []
 paused = False
+prev_time = time.time()
+prev_wait_time = 0
+time_since = 120
+typing_time = 0
+message_time_sent = False
+grace_period = True
+grace_timestamp = None
+additional_wait = 0
+grace_time = 0
+c = 0
 
 # Subclass fbchat.Client and override required methods
 class ChatBot(Client):
@@ -29,15 +39,17 @@ class ChatBot(Client):
         if (is_command) and (reply != ""):
             print(reply)
         elif (not is_command):
+            neuralnetworkresponse(self, author_id, message_object, thread_id, thread_type, admin, **kwargs)
             print("not a command, handle it as a message instead")
         else:
             print("it was a command and handled already")
-        self.read_message_function(thread_id, **kwargs)
+            self.read_message_function(self, author_id, message_object, thread_id, thread_type, reply, admin, **kwargs)
 
-    def read_message_function(self, thread_id, **kwargs):
+    def read_message_function(self, author_id, message_object, thread_id, thread_type, reply, admin, **kwargs):
         self.markAsRead(thread_id)
 
     def send_async_message(self, author_id, message_object, thread_id, thread_type, reply, typing_time, **kwargs):
+        global message_time_sent
         # Will set the typing status of the thread to `TYPING`
         client.setTypingStatus(TypingStatus.TYPING, thread_id=thread_id, thread_type=thread_type)
         time.sleep(typing_time) #type for a realistic time, if it types for too long, I need to recall it because it times out
@@ -162,6 +174,53 @@ def resume(self, author_id, message_object, thread_id, thread_type, arguments, a
                 self.send(Message(text="resumed!"), thread_id=thread_id, thread_type=thread_type)
     else:
         self.send(Message(text="You're not admin!"), thread_id=thread_id, thread_type=thread_type)
+
+def neuralnetworkresponse(self, author_id, message_object, thread_id, thread_type, admin, **kwargs):
+    global paused_id, resumed_id, paused, prev_time, prev_wait_time, time_since, typing_time, message_time_sent, grace_period, grace_timestamp, additional_wait, wait_for
+    if (thread_id in paused_id) or ((paused == True) and (thread_id not in resumed_id)):
+        status = False
+    else: #if it's not in paused or if it is globally paused and this thread is resumed
+        status = True #send message confirmed
+    
+    if (author_id != self.uid) and (status == True): #only processes the messages if not paused
+        current_time = time.time()
+        if time_since < prev_wait_time: #handle all these variables on a user by user basic in a dict maybe
+            additional_wait = prev_wait_time - time_since + typing_time
+        print("H:",message_object.text)
+        reply = chatbot_recieve(message_object.text)
+        lower_bound_sleep = len(reply)*139 #ms between each character on avg
+        upper_bound_sleep = int(lower_bound_sleep * 1.5)
+        typing_time = randint(lower_bound_sleep,upper_bound_sleep)/1000
+        if message_time_sent:
+            time_since = current_time - message_time_sent
+            print(time_since)
+        if grace_period == False:
+            wait_for = randint(1,2) + additional_wait
+            print(wait_for)
+            t = threading.Timer(wait_for, ChatBot.send_async_message, [self, author_id, message_object, thread_id, thread_type, reply, typing_time])
+            t.start()  # after wait_for seconds, the message will begin to be sent
+        elif time_since < 60:
+            wait_for = randint(3,5) + additional_wait
+            print(wait_for)
+            t = threading.Timer(wait_for, ChatBot.send_async_message, [self, author_id, message_object, thread_id, thread_type, reply, typing_time])
+            t.start()  # after wait_for seconds, the message will begin to be sent
+        else:
+            wait_for = randint(5,8) + additional_wait
+            print(wait_for)
+            t = threading.Timer(wait_for, ChatBot.send_async_message, [self, author_id, message_object, thread_id, thread_type, reply, typing_time])
+            t.start()  # after wait_for seconds, the message will begin to be sent
+        read_message = threading.Timer(int(wait_for/2), ChatBot.read_message_function, [self, author_id, message_object, thread_id, thread_type, reply, admin])
+        print("Seen wait:",int(wait_for/2))
+        read_message.start()
+        grace_period = False
+        grace_time = randint(12,30)
+        grace_timestamp = time.time()
+        print("Grace_time:",grace_time)
+        count_down = threading.Timer(grace_time, ChatBot.grace_period_counter)
+        count_down.start()
+        c = 0
+        prev_time = time.time()
+        prev_wait_time = wait_for
 
 client = ChatBot("", "")
 client.listen()
